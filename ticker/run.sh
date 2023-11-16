37c1832565ce451ed7e873cd0cc7cb93c559b1b1
Robert Heeter <robert.heeter@rice.edu>
2023-11-16 15:09:40 -0600

    Update docs

--- ticker/run.sh
+++ ticker/run.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# Run /projects/ticker/ticker.py automatically on boot with cron as follows:
-
-# cd /var/lib/cloud9                  [move to cloud9 folder]
-# mkdir logs                          [make logs folder]
-# sudo chmod 777 /var/lib/cloud9/logs [modify permissions for logs folder]
-# sudo crontab -e                     [open crontab]
-# -                                   [make modifications; add this line:
-#                                         @reboot sleep 60 && sh /var/lib/cloud9/projects/ticker/run.sh > /var/lib/cloud9/logs/cronlog 2>&1
-#                                     ]
-# ^X                                  ["ctrl+X" to exit editor]
-# Y                                   ["Y" to save modified buffer]
-# -                                   ["enter" to confirm file name]
-# -                                   [shut off and restart the PocketBeagle to make cron changes]
+# Run /projects/ticker/ticker.py automatically on boot with cron.
+# See documentation at https://github.com/rcheeter/ticker for setting up cron.
 
 cd /var/lib/cloud9/projects/ticker
 sudo python3 ticker.py
